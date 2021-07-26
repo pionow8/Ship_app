120c06e9e328565b7f59a0ec081629eccac02d61
Piotr <pio.nowosielski@gmail.com>
2021-07-26 20:36:12 +0200

    delete add external resources

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,8 +10,7 @@
 #' @noRd
 app_ui <- function(request) {
   tagList(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
+    
     # Your application UI logic 
     shinydashboard::dashboardPage(
       shinydashboard::dashboardHeader(title = "Ship application"),
@@ -35,31 +34,6 @@ app_ui <- function(request) {
         )
       )
     )
-  )
-}
-
-#' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
-#' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
-golem_add_external_resources <- function(){
-  
-  add_resource_path(
-    'www', app_sys('app/www')
-  )
-  
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'ShipApp'
-    )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
   )
 }
 
